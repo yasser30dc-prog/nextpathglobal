267b6602a3aa60bf89283428b8a31bd2c0f82be1
NextPath Global <yasser30dc-prog@github.com>
2025-12-19 17:18:31 +0800

    Update contact address with full details

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -65,8 +65,9 @@ export default function Footer() {
                     <div>
                         <h3 className="text-lg font-bold mb-6 text-primary">Contact</h3>
                         <ul className="space-y-3 text-gray-600">
-                            <li>Vista Tower, The Intermark</li>
-                            <li>Kuala Lumpur, Malaysia</li>
+                            <li>Level 41, Vista Tower, The Intermark</li>
+                            <li>384, Jln Tun Razak, Kampung Datuk Keramat</li>
+                            <li>50400 Kuala Lumpur, Malaysia</li>
                             <li>+60 11 1669 5249</li>
                             <li>hello@nextpathglobal.my</li>
                         </ul>
